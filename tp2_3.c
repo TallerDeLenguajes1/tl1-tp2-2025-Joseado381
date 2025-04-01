c8b784229d77eed398e8b41778e768dd9bd8e270
Joseado381 <josealonzo.jaa@gmail.com>
2025-04-01 14:01:50 -0300

    se modifico con punteros el punto3

--- tp2_3.c
+++ tp2_3.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define N 5
 #define M 7
@@ -7,13 +9,23 @@ int main()
 {
     int i,j;
     int mt[N][M];
+    int *p_mt=&mt[0][0];
+    srand(time(NULL));
 
     for(i = 0;i<N; i++)
     {
         for(j = 0;j<M; j++)
         {
-            mt[i][j]=1+rand()%100;
-            printf("%lf ", mt[i][j]);
+            *(p_mt + i*M + j)=1+rand()%100;
+            if (*(p_mt + i*M + j) < 10)
+            {
+                printf("[0%d] ", *(p_mt + i*M + j));
+            }else
+            {
+                printf("[%d] ", *(p_mt + i*M + j));
+            }
+            
+            
         }
         printf("\n");
     }
